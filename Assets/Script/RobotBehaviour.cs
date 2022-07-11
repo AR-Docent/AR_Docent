cd84fdc668de3317bcdb35c29cee3c2cf04ec4ef
hongsam14 <69339846+hongsam14@users.noreply.github.com>
2022-07-12 00:22:11 +0900

    use depth, mesh of ARKit

---
+++ Assets/Script/RobotBehaviour.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotBehaviour : MonoBehaviour
+{
+    Vector3 start_position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        start_position = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
