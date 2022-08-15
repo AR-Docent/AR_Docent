23112b02694e84d1ccecdaaef16a64a43d675592
hongsam14 <69339846+hongsam14@users.noreply.github.com>
2022-08-16 01:58:18 +0900

    mouse click

---
+++ Assets/Scripts/RaycastClickEvent.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastClickEvent : MonoBehaviour
+{
+    private Camera _mainCam;
+
+    private Ray _ray;
+    private RaycastHit _hit;
+
+    public bool clicked { get; private set; } = false;
+    public Action<bool> clickEvent;
+
+    void Awake()
+    {
+        _mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _ray = _mainCam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(_ray, out _hit, 100f))
+            {
+                if (_hit.transform == this.transform)
+                {
+                    clicked = !clicked;
+                    clickEvent(clicked);
+                }
+            }
+        }
+    }
+}
