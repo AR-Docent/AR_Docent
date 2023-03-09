fd97f740f3f5c4d165b81abe547515af9a4b64a3
hongsam14 <69339846+hongsam14@users.noreply.github.com>
2023-03-09 12:57:52 +0900

    make get method in networkManager

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkManager
+{
+
+    private static T FromByteArray<T>(byte[] data)
+    {
+        T ret = default;
+
+        if (data == null)
+        {
+            return ret;
+        }
+        BinaryFormatter binary = new ();
+        using MemoryStream ms = new(data);
+
+        object obj = binary.Deserialize(ms);
+        ret = (T)obj;
+
+        return ret;
+    }
+    public static T GetData<T>(string url)
+    {
+        T ret = default;
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            UnityWebRequestAsyncOperation oper = request.SendWebRequest();
+            while (!oper.isDone)
+            {
+            }
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError($"{request.error} : {request.url}");
+                throw new Exception($"{request.error} : {request.url}");
+            }
+            ret = FromByteArray<T>(request.downloadHandler.data);
+        }
+        return ret;
+    }
+
+    /*
+    public static async T GetDataAsync<T>(string url)
+    {
+        T ret = default;
+        return ret;
+        
+    }
+    */
+}
